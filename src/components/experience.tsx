dec00cde71bf94d41bf37a28460f51d9aa02e3ba
Uzair Ahmed <uzairmahmed@gmail.com>
2025-02-09 17:19:23 -0500

    experiences

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -64,9 +64,6 @@ const Experience: React.FC = () => {
       duration: "Jan 2025 - Present",
       location: "Norfolk, VA",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
       ],
       technologies: [
         "OpenAI",
@@ -85,9 +82,10 @@ const Experience: React.FC = () => {
       duration: "January 2024 - December 2024",
       location: "Raleigh-Durham, NC",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Led a team in creating a virtual multi-specialty cancer support platform, enabling patient intake, scheduling video consultations with specialists via Zoom Video SDK, and processing payments through Stripe. Developed patient, admin, and specialist dashboards, integrating the platform with CharmHealth EHR for reliable operation.",
+        "Redesigned the platform to enhance usability, reliability, and security, and integrated a data synchronization pipeline with CharmHealth APIs",
+        "Migrated the platform from AWS to Google Cloud, implementing Google App Engine and MongoDB Atlas, and establishing a CI/CD process with Google Cloud Build and Cloud Run. This transition reduced monthly costs by 80% and eliminated deployment and scalability issues",
+        "Designed and built the company's frontend landing page, aligning with the client's vision and delivering a user-friendly interface",
       ],
       technologies: [
         "React.js",
@@ -106,9 +104,12 @@ const Experience: React.FC = () => {
       duration: "May 2023 - January 2024",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Built a comprehensive web application using ReactJS and Django for corporate catering and events management, enabling clients to schedule events and receive catering quotations from restaurants based on their specifications",
+        "Developed a customer-facing dashboard for real-time updates and integrated mobile notifications with Twilio and SendGrid, and online payments using Stripe, enhancing customer engagement and a seamless user experience",
+        "Created an operations-facing dashboard for staff members to manage orders, invoicing, administration, and communication between clients and restaurants",
+        "Overhauled DevOps infrastructure by containerizing the application with Docker, providing developers with a mirrored test environment, achieving nearly zero downtime for maintenance through CI/CD practices and enabling efficient resource scaling",
+        "Participated in design meetings and attended events to gain firsthand experience, resulting in a well-informed app design that met user needs and industry standards",
+        "Led development team through scrum sprints and ticket assignments, ensuring efficient project completion",
       ],
       technologies: [
         "React.js",
@@ -128,9 +129,9 @@ const Experience: React.FC = () => {
       duration: "May 2022 - September 2022",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Worked as part of a team managing a network of approximately 300 servers, ensuring seamless operation and reliability across various sectors of the bank",
+        "Developed a modern analytics and monitoring dashboard using Datadog, enabling real-time monitoring and detailed historical data analysis",
+        "Integrated Datadog with ServiceNow to automate issue ticket creation, ensuring prompt incident resolution by directing tickets to the appropriate personnel",
       ],
       technologies: [
         "Datadog",
@@ -149,9 +150,11 @@ const Experience: React.FC = () => {
       duration: "September 2022 - May 2022",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Developed and deployed a React Native app and Django backend that provided live wait times for local businesses during the COVID-19 pandemic, later transitioning into a check-in/booking system for restaurant reservations and featured nearby locations",
+        "Integrated an AI model to estimate wait times using various APIs such as Google Places, Weather APIs and Google Maps",
+        "Built and maintained APIs to support the app's functionality and ensured seamless integration with our backend infrastructure",
+        "Implemented a comprehensive user system, allowing users to book reservations, sign up, save and search businesses",
+        "Trained other interns on the codebase and added new features to both the backend and frontend, ensuring continuous improvement and functionality of the app.",
       ],
       technologies: [
         "React Native",
@@ -170,9 +173,10 @@ const Experience: React.FC = () => {
       duration: "September 2020 - February 2022",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Collaborated with a team focused on developing accessible technologies for individuals with hearing impairments, motor disabilities, dysarthria, and other conditions requiring assistive tech",
+        "Integrated ESP32 and BLE SPI technologies with voice recognition libraries to enable users to interact with home IoT devices and mobile phones through voice commands",
+        "Adapted voice recognition systems to accommodate dysarthric patients, ensuring their voices could be accurately recognized and processed by the devices",
+        "Assisted a master's student in training a PyTorch model to better understand and identify keywords spoken by dysarthric patients, enhancing the accuracy of voice recognition",
       ],
       technologies: [
         "ESP32 Microcontrollers",
@@ -191,9 +195,9 @@ const Experience: React.FC = () => {
       duration: "May 2021 - September 2021",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Contributed to the development and maintenance of a web application using ReactJS for the frontend and .NET for the backend, enhancing features to support the investment project management team in requesting, approving, and tracking projects",
+        "Integrated user roles and permissions with the bank's SAML SSO system, ensuring secure and efficient access control",
+        "Participated in meetings with end-users to gather requirements, understand feature requests, and design solutions based on user feedback and recommendations",
       ],
       technologies: ["ASP.NET", "SQL", "React.js", "C#", "Jenkins"],
     },
@@ -204,9 +208,10 @@ const Experience: React.FC = () => {
       duration: "May 2019 - September 2019",
       location: "Toronto, ON",
       description: [
-        "Led development of key features resulting in 30% user growth",
-        "Implemented responsive design patterns using React and TypeScript",
-        "Collaborated with cross-functional teams to deliver projects on time",
+        "Worked in the system integrations team, integrating military infrared cameras with object detection systems",
+        "Implemented a containerized network using Docker, with containers for pulling camera feeds, running object detection, and displaying live results",
+        "Trained a TensorFlow object detection model on infrared camera images to identify various types of vehicles",
+        "Integrated the trained model with the camera’s live feed to perform real-time object detection, displaying tagged objects on a Flask web application in real-time",
       ],
       technologies: [
         "Docker",
